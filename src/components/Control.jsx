2170b98aedcbd3ca88357d8d6c3688c7e2b9ea05
yoonbumtae <yoonbumtae@gmail.com>
2020-12-08 19:43:42 +0900

    ㅁㅁㅇㄴ

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -5,18 +5,19 @@ class Control extends Component {
         // alert(this.props.mode)
         return (
             <ul className="control-list">
-                <li><a href="create" className="button" onClick={e => {
+                <li><button className="button" onClick={e => {
                     this.props.onClick('CREATE')
                     e.preventDefault()
-                }}>create</a></li>
+                }}>create</button></li>
                 {(this.props.mode !== 'WELCOME' && this.props.selectedId !== 0) &&
                     <>
-                        <li><a href="update" className="button" onClick={e => {
+                        <li><button className="button" onClick={e => {
                             this.props.onClick('UPDATE')
                             e.preventDefault()
-                        }}>update</a></li>
-                        <li><button className="button-delete" onClick={e => {
-                            // 삭제는 a href 로 구현하면 안된다.
+                        }}>update</button></li>
+                        <li><button className="button delete" onClick={e => {
+                            // 생성, 업데이트는 a href 구현이 가능하나 삭제는 a href 로 구현하면 안된다.
+                            // 어차피 이벤트를 사용할거면 버튼을 쓰는게 나으므로 셋 다 버튼으로 통일
                             this.props.onClick('DELETE_PROCESS')
                             e.preventDefault()
                         }}>delete</button></li>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ class Nav extends Component {
         const tags = []
         // console.log(this)
         for(let data of this.props.data) {
-            tags.push(<li key={data.id}><a href="#" data-id={data.id} onClick={e => {
+            tags.push(<li key={data.id}><a href="/#" data-id={data.id} onClick={e => {
                 // alert(data.id)
                 // console.log("inner this", this, this.props, this.props.onClick)
                 this.props.onClick(+e.target.dataset.id)
